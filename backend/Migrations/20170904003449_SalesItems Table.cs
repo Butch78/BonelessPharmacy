5e60317da8fad666c2cab0f6fd200b4a3aefa107
pielegacy <alex_billson@outlook.com>
2017-09-04 10:37:44 +1000

    First migration, added watcher to csproj, updated gitignore

---
+++ backend/Migrations/20170904003449_SalesItems Table.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace BonelessPharmacyBackend.Migrations
+{
+    public partial class SalesItemsTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "SalesItems",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Amount = table.Column<int>(type: "INTEGER", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    Price = table.Column<double>(type: "REAL", nullable: false),
+                    StockOnHand = table.Column<uint>(type: "INTEGER", nullable: false),
+                    SupplierCode = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SalesItems", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SalesItems");
+        }
+    }
+}
