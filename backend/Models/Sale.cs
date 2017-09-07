148e6e0e6f483a03b4016bcc4f86a29db860bb9e
Alex Billson <strikingmongoose@hotmail.com>
2017-09-07 16:16:13 +1000

    Modelled sales record, placehold for sale

---
+++ backend/Models/Sale.cs
@@ -0,0 +1,11 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BonelessPharmacyBackend
+{
+    public class Sale
+    {
+        
+    }
+}
---
+++ backend/Models/SalesRecord.cs
@@ -0,0 +1,51 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BonelessPharmacyBackend
+{
+    public class SalesRecord
+    {
+        [Key]
+        /// <summary>
+        /// The primary key of the SalesRecord
+        /// </summary>
+        /// <returns></returns>
+        public int Id { get; set; }
+        
+        [Required]
+        /// <summary>
+        /// The foreign key of the related Sale
+        /// </summary>
+        /// <returns></returns>
+        public int SaleId { get; set; }
+
+        [ForeignKey("SaleId")]
+        /// <summary>
+        /// The foreign object for the Sale
+        /// </summary>
+        /// <returns></returns>
+        public Sale Sale { get; set; }
+
+        [Required]
+        /// <summary>
+        /// The foreign key of the related SalesItem
+        /// </summary>
+        /// <returns></returns>
+        public int ItemId { get; set; }
+
+        [ForeignKey("ItemId")]
+        /// <summary>
+        /// The foreign object of the sales item
+        /// </summary>
+        /// <returns></returns>
+        public SalesItem SalesItem { get; set; }
+
+        [Required]
+        /// <summary>
+        /// The amount of the sale item purchased
+        /// </summary>
+        /// <returns></returns>
+        public int Quantity { get; set; }
+    }
+}
