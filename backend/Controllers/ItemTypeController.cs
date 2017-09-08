a0d6a76b97503f6daa52c621be0d4be7dcba8996
coreyjenkins <101101551@student.swin.edu.au>
2017-09-08 20:41:38 +1000

    ItemTypeController Finish
    Completed ItemTypeController

--- backend/Controllers/ItemTypeController.cs
+++ backend/Controllers/ItemTypeController.cs
@@ -18,5 +18,60 @@ namespace BonelessPharmacyBackend.Controller
                 return db.ItemType.ToList();
             }
         });
+        
+        //GET api/ItemType
+        [HttpGet("{id}")]
+        public async Task<ItemType> Get(int id) => await Task.Run<ItemType>(() =>
+        {
+            using (var db = new Db())
+            {
+                return db.ItemType.FirstOrDefault(s => s.Id == id);
+            }
+        });
+
+        //POST api/ItemType
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody]ItemType value) => await Task.Run<IActionResult>(async () =>
+        {
+            if (ModelState.IsValid)
+            {
+                using (var db = new Db())
+                {
+                    await db.ItemType.AddAsync(value);
+                    await db.SaveChangesAsync();
+                    return Created("api/ItemType", value);
+                }
+            }
+            else
+                return BadRequest();
+        });
+
+        //PUT api/ItemType
+        public async Task<IActionResult> Put(int id, [FromBody]ItemType value) => await Task.Run<IActionResult>(async () =>
+        {
+            if (ModelState.IsValid)
+            {
+                value.Id = id;
+                using (var db = new Db())
+                {
+                    db.ItemType.Update(value);
+                    await db.SaveChangesAsync();
+                    return Accepted("api/ItemType", value);
+                }
+            }
+            else
+                return BadRequest();
+        });
+
+        // DELETE api/ItemType
+        public async Task<IActionResult> Delete(int id) => await Task.Run<IActionResult>(async () =>
+        {
+            using (var db = new Db())
+            {
+                db.ItemType.Remove(db.ItemType.FirstOrDefault(s => s.Id == id));
+                await db.SaveChangesAsync();
+                return Accepted();
+            }
+        });
     }
 }
