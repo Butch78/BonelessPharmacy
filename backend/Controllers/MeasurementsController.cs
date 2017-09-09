6e70e745238eeb2ba5cd47a9b5dd949632dc9d11
pielegacy <alex_billson@outlook.com>
2017-09-09 18:47:25 +1000

    Trying to get JWT to work but it ain't

--- backend/Controllers/MeasurementsController.cs
+++ backend/Controllers/MeasurementsController.cs
@@ -2,10 +2,13 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BonelessPharmacyBackend.Controllers
 {
+    // [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
     [Route("api/[controller]")]
     public class MeasurementsController : Controller
     {
