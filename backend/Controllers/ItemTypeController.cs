bdd200a3b6568ec5a2faf6634fd3ec41d2f23556
pielegacy <alex_billson@outlook.com>
2017-09-09 21:03:17 +1000

    Added fix for my issues

--- backend/Controllers/ItemTypeController.cs
+++ backend/Controllers/ItemTypeController.cs
@@ -4,19 +4,20 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
-namespace BonelessPharmacyBackend.Controller
+namespace BonelessPharmacyBackend.Controllers
 {
     [Route("api/[controller]")]
     public class ItemTypeController: Controller
     {
+        // TODO: Actually implement itemtype
         // GET api/ItemType
-        [HttpGet]
-        public async Task<IEnumerable<ItemType>> Get() => await Task.Run<IEnumerable<SalesItem>>(() =>
-        {
-            using (var db = new Db())
-            {
-                return db.ItemType.ToList();
-            }
-        });
+        // [HttpGet]
+        // public async Task<IEnumerable<ItemType>> Get() => await Task.Run<IEnumerable<SalesItem>>(() =>
+        // {
+        //     using (var db = new Db())
+        //     {
+        //         return db.ItemType.ToList();
+        //     }
+        // });
     }
 }
