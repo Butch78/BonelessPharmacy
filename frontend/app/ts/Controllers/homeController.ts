41df981fd634a485382a44be7ddb63dabc22b05f
pielegacy <alex_billson@outlook.com>
2017-09-11 17:51:44 +1000

    Added boneless stuff

--- frontend/app/ts/Controllers/homeController.ts
+++ frontend/app/ts/Controllers/homeController.ts
@@ -1,8 +1,11 @@
 /**
  * Home Controller
- * 
  * Controller associated with the home page of the application
  */
-app.controller("homeCtrl", ($scope) => {
+app.controller("homeCtrl", ($scope, $http) => {
     $scope.name = "Alex";
+    $http({
+        method: 'GET',
+        url: `${Boneless.API_ENDPOINT}Measurements`,
+    }).then((res) => console.log(res));
 });
---
+++ frontend/app/ts/boneless.ts
@@ -0,0 +1,9 @@
+/**
+ * Singleton helper for the application
+ */
+class Boneless {
+    /**
+     * The current endpoint to be querying with a '/' appended
+     */
+    public static readonly API_ENDPOINT: string = "http://localhost:5000/";
+}
