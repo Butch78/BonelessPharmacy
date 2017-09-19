2df203a3d658f8b02c634105f351ab2e53ef8897
ChoiceKarpz <campbell_graham@outlook.com>
2017-09-19 12:40:32 +1000

    Table for sales drafted along with popup for extra information

--- frontend/app/ts/Controllers/salesController.ts
+++ frontend/app/ts/Controllers/salesController.ts
@@ -10,4 +10,9 @@ app.controller("salesCtrl", ($scope) => {
     $scope.openModalNewSale = function() {
         $('#modalNewSale').modal('open');
     }
+
+    //will need to be adjusted later so that it takes in the ID of a sale, meaning it can present the relevant details
+    $scope.openModalSaleDetails = function() {
+        $('#modalSaleDetails').modal('open');
+    }
 });
