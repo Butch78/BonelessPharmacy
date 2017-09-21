8728eaedf52c1d1960c29c46530ef3b7edddb655
Alex Billson <strikingmongoose@hotmail.com>
2017-09-21 17:23:25 +1000

    Added edit button and modified tables

--- frontend/app/ts/Controllers/salesController.ts
+++ frontend/app/ts/Controllers/salesController.ts
@@ -1,6 +1,5 @@
 /**
  * Home Controller
- * 
  * Controller associated with the home page of the application
  */
 app.controller("salesCtrl", ($scope, $http) => {
@@ -16,12 +15,12 @@ app.controller("salesCtrl", ($scope, $http) => {
             alert(errorRes.data);
     });
 
-    $scope.openModalNewSale = function() {
+    $scope.openModalNewSale = () => {
         $('#modalNewSale').modal('open');
-    }
+    };
 
-    //will need to be adjusted later so that it takes in the ID of a sale, meaning it can present the relevant details
-    $scope.openModalSaleDetails = function() {
+    // Will need to be adjusted later so that it takes in the ID of a sale, meaning it can present the relevant details
+    $scope.openModalSaleDetails = () => {
         $('#modalSaleDetails').modal('open');
-    }
+    };
 });
