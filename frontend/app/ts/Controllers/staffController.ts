1a8c93fb9ac8c714500f78a8f869492bb4f2044a
shanejoachim <shanejoachim41@gmail.com>
2017-09-21 18:25:00 +1000

    Basic Staff Controller

---
+++ frontend/app/ts/Controllers/staffController.ts
@@ -0,0 +1,16 @@
+/**
+ * Staff Controller
+ * Controller associated with the staff page of the application
+ */
+app.controller("staffCtrl", ($scope, $http) => {
+
+    // GET Staff details
+    $http(Boneless.CreateRequest("api/Staff", "get")).then(
+        (res) => {
+            $scope.staff = res.data as Staff[];
+        },
+        (errorRes) => {
+            alert(errorRes.data);
+        });
+
+});
