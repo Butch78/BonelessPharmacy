24b916fe45225647cdaf5727a221b8013d25e9e2
Matthew <matthewtyleraylward@hotmail.com>
2017-10-02 20:34:14 +1100

    1st Draft of Help page

--- frontend/app/ts/Controllers/helpController.ts
+++ frontend/app/ts/Controllers/helpController.ts
@@ -7,9 +7,16 @@ app.controller("helpCtrl", ($scope) => {
 
 
     $scope.HelpItems = [{
-    title:"Creating a Sale", 
-    desc: ["1", "2"]
-    }];
+    title:"Logging In", 
+    desc: ["1. Type in your Username and Password", "2. Press Login"]
+    },{
+        title:"Creating a Sale", 
+        desc: ["1. Press the plus button in the bottom right corner", "2.Add the Sale items", "3.Complete the Transaction"]
+    }, {
+        title:"Editing a Sale", 
+        desc: ["1. Press on the sale you wish to edit", "2. Edit the sale and press save changes"]
+    }
+];
 
     //Create an Array of strings with <br> behind it 
 });
