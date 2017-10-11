8d3339af8060c941371c7b369cb477b5708a3353
Alex Billson <alex_billson@outlook.com>
2017-10-12 10:02:58 +1100

    It actually authenticates now hahah

--- backend/Controllers/StaffController.cs
+++ backend/Controllers/StaffController.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Bogus;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -16,65 +18,69 @@ namespace BonelessPharmacyBackend.Controllers
         {
             using (var db = new Db())
             {
-            // Ensure to call toList so that the DB doesn't dispose itself
-            return db.Staff.Include(s => s.Role).Select(s => s.Safe).ToList();
+                // Ensure to call toList so that the DB doesn't dispose itself
+                return db.Staff.Include(s => s.Role).Select(s => s.Safe).ToList();
             }
         });
- 
-    //Get api/Staff/5
-    [HttpGet("{id}")]
-    public async Task<Staff> Get(int id) => await Task.Run<Staff>(() =>
-    {
-        using (var db = new Db())
-        {
-            return db.Staff.Include(s => s.Role).Select(s => s.Safe).FirstOrDefault(s => s.Id == id);
-        }
-    });
 
-    //  Post api/Staff/5
-    [HttpPost]
-    public async Task<IActionResult> Post([FromBody]Staff value) => await Task.Run<IActionResult>(async ()   =>
-    {
-        if(ModelState.IsValid)
+        //Get api/Staff/5
+        [HttpGet("{id}")]
+        public async Task<Staff> Get(int id) => await Task.Run<Staff>(() =>
         {
             using (var db = new Db())
             {
-                await db.Staff.AddAsync(value); 
-                await db.SaveChangesAsync();
-                return Created("api/Staff", value);
+                return db.Staff.Include(s => s.Role).Select(s => s.Safe).FirstOrDefault(s => s.Id == id);
             }
-        }
-        else 
-            return BadRequest();
-    });
+        });
 
-    [HttpPut("{id}")]
-    public async Task<IActionResult> Put(int id, [FromBody]Staff value) => await Task.Run<IActionResult>(async () =>
-    {
-        if(ModelState.IsValid)
+        //  Post api/Staff/5
+        [HttpPost]
+        [Authorize]
+        public async Task<IActionResult> Post([FromBody]Dictionary<string, string> value) => await Task.Run<IActionResult>(async () =>
         {
-            value.Id = id; 
-            using(var db = new Db())
+            if (!value.ContainsKey("name") || !value.ContainsKey("phone") || !value.ContainsKey("roleid"))
+                return BadRequest("Staff creation requires name and phone number");
+            string secret = $"{new Faker().Lorem.Word()}{new Random().Next(999)}";
+            using (var db = new Db())
             {
-                db.Staff.Update(value);
+                await db.Staff.AddAsync(new Staff(){
+                    Name = value["name"],
+                    Password = secret,
+                    RoleId = Int32.Parse(value["roleid"]),
+                    PhoneNumber = value["phone"]
+                });
                 await db.SaveChangesAsync();
-                return Accepted("api/Staff", value);
+                return Created("api/Staff", secret);
             }
-        }
-        else 
-            return BadRequest();
-    });
+        });
 
-    [HttpDelete("{id}")]
-    public async Task<IActionResult> Delete(int id) => await Task.Run<IActionResult>(async ()   =>
-    {
-        using (var db = new Db())
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put(int id, [FromBody]Staff value) => await Task.Run<IActionResult>(async () =>
         {
-            db.Staff.Remove(db.Staff.FirstOrDefault(s => s.Id == id));
-            await db.SaveChangesAsync();
-            return Accepted();
-        }
-    });
+            if (ModelState.IsValid)
+            {
+                value.Id = id;
+                using (var db = new Db())
+                {
+                    db.Staff.Update(value);
+                    await db.SaveChangesAsync();
+                    return Accepted("api/Staff", value);
+                }
+            }
+            else
+                return BadRequest();
+        });
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id) => await Task.Run<IActionResult>(async () =>
+        {
+            using (var db = new Db())
+            {
+                db.Staff.Remove(db.Staff.FirstOrDefault(s => s.Id == id));
+                await db.SaveChangesAsync();
+                return Accepted();
+            }
+        });
 
     }
 }
